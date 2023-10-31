b32ed2e66048562f0f3d56e63671a78bb15aedc1
hominhk3 <144079318+hominhk3@users.noreply.github.com>
2023-10-31 16:42:37 +0700

    Add files via upload

---
+++ SearchFIle/src/Main.java
@@ -0,0 +1,12 @@
+import controller.SearchFileManagement;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        new SearchFileManagement().run();
+    }
+}
